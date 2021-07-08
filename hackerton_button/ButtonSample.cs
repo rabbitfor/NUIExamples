57f15984f8646ad63eaaff6bff6136aebf195d75
Jiyun Yang <ji.yang@samsung.com>
2021-07-09 00:51:41 +0900

    Add hackerton button example
    Signed-off-by: Jiyun Yang <ji.yang@samsung.com>

---
+++ hackerton_button/ButtonSample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Components;
+
+namespace ButtonSample
+{
+    class Program : NUIApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+
+            // NOTE To use theme.xaml, uncomment below line.
+            // ThemeManager.ApplyTheme(new Theme(Tizen.Applications.Application.Current.DirectoryInfo.Resource + "theme/theme.xaml"));
+
+            XamlPage page = new XamlPage();
+            Window.Instance.Add(page);
+        }
+
+        public void OnKeyEvent(object sender, Window.KeyEventArgs e)
+        {
+            if (e.Key.State == Key.StateType.Down && (e.Key.KeyPressedName == "XF86Back" || e.Key.KeyPressedName == "Escape"))
+            {
+                Exit();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Run(args);
+        }
+    }
+}
